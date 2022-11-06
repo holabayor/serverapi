edfc67b9dd764033c3c5cae034e3a0a6f5ebcc75
holabayor <liasu.olabayo@gmail.com>
2022-11-06 17:10:20 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import enum
+import json
 
 
 app = Flask(__name__)
@@ -13,6 +15,24 @@ def home():
             'backend': True,
             'age': 28,
             'bio': 'I love writing functional codes on the backend'}
+    return jsonify(info)
+
+@app.route('/calculate', methods=['POST'], strict_slashes=False)
+def operation():
+    data = request.get_json()
+    x = int(data.get('x'))
+    y = int(data.get('y'))
+    operator_type = data.get('operator_type')
+    if operator_type in ('+', 'addition', 'add', 'plus'):
+        result = x + y
+    if operator_type in ('-', 'subtraction', 'subtract', 'minus'):
+        result = x - y
+    if operator_type in ('*', 'multiplication', 'multiply'):
+        result = x * y
+
+    info = {'slackUsername': 'OlaJosh',
+            'operation_type': operator_type,
+            'result': result}
     return jsonify(info)
 
 
